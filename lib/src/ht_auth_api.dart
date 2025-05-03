3efd1b4431b35c9e3b052199b3b0d79249ae1dc1
fulleni <fulleni@hotmail.com>
2025-05-03 12:00:13 +0100

    chore(ht_auth_api): create initial project structure
    - Set up basic project layout and configuration
    - Add issue templates and PR checklist
    - Configure CI/CD and dependency management
    - Include sample code and test

---
+++ lib/src/ht_auth_api.dart
@@ -0,0 +1,7 @@
+/// {@template ht_auth_api}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtAuthApi {
+  /// {@macro ht_auth_api}
+  const HtAuthApi();
+}
---
+++ test/src/ht_auth_api_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_auth_api/ht_auth_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtAuthApi', () {
+    test('can be instantiated', () {
+      expect(HtAuthApi(), isNotNull);
+    });
+  });
+}
