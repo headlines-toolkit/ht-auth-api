d5d3af1f4800b2f10f0ff4c610750d076265f866
fulleni <fulleni@hotmail.com>
2025-05-04 04:49:15 +0100

    docs: update package description and library documentation
    - Update the package description to accurately reflect its purpose and functionality
    - Enhance library documentation to provide clearer context for its usage

--- lib/ht_auth_api.dart
+++ lib/ht_auth_api.dart
@@ -1,4 +1,7 @@
-/// A Very Good Project created by Very Good CLI.
+/// Concrete API implementation of the HtAuthClient interface defined in
+/// `package:ht_auth_client`. This package provides the logic for interacting
+/// with a backend authentication service via HTTP requests using
+/// `package:ht_http_client`.
 library;
 
 export 'src/ht_auth_api.dart';
